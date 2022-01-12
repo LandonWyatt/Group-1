20ba69929f374e1876959c4178429ffdd34aa663
Landon Wyatt <landon@test.com>
2022-01-12 09:42:15 -0500

    product page additions

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,11 +1,4 @@
-window.onload = function() {
-
-	if (window.addEventListener)
-		window.addEventListener('keyup', takeInput, false);
-
-	function takeInput(e) {
-		var searchBar = document.getElementById('searchBar');
-		var val = searchBar.value;
-		console.log(val);
-	}
+function takeInput() {
+	var search = document.getElementById("searchBar");
+	console.log(search.value);
 }
