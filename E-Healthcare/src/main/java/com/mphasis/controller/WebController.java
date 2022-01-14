14ad9303333a31dc39d79a579fbe0e366ab639a1
Landon Wyatt <landon@test.com>
2022-01-14 17:31:26 -0500

    work on product table

--- E-Healthcare/src/main/java/com/mphasis/controller/WebController.java
+++ E-Healthcare/src/main/java/com/mphasis/controller/WebController.java
@@ -1,6 +1,7 @@
 package com.mphasis.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mphasis.model.Product;
 
@@ -16,6 +19,7 @@ public class WebController {
 	
 	@Autowired
 	private ProductController productController;
+	private int numEntries = 10;
 	
 	@GetMapping("/")
 	public String getHome() {
@@ -28,14 +32,37 @@ public class WebController {
 	}
 	
 	@GetMapping("/product")
-	public String getProduct(Model model) {
+	public ModelAndView getProduct(Map<String, Object> model) {
 		// user_product or admin_product needs to be determined and decide which to return
-//		int numEntries = 5;
-		System.out.println("change occurred");
+		System.out.println("/product mapping visited");
 		List<Product> productsList = productController.getAllProducts();
-		model.addAttribute("products", productsList);
-		return "admin_product";
+		System.out.println("productsList: " + productsList);
+		model.put("numChosen", numEntries);
+		System.out.println("numChosen: " + numEntries);
+		model.put("products", productsList);
+		return new ModelAndView("admin_product");
 	}
+	
+	@PostMapping("/productChange")
+	public ModelAndView getProductOrganization(Map<String, Object> model, @RequestBody Map<String, String> data) {
+		// user_product or admin_product needs to be determined and decide which to return
+		numEntries = Integer.parseInt(data.get("numChosen"));
+		System.out.println(numEntries);
+//		List<Product> productsList = productController.getAllProducts();
+//		model.put("numChosen", numChosen);
+//		model.put("products", productsList);
+		return new ModelAndView("redirect:/product");
+	}
+	
+//	@GetMapping("/product")
+//	public String getProduct(Model model) {
+//		// user_product or admin_product needs to be determined and decide which to return
+////		int numEntries = 5;
+//		System.out.println("change occurred");
+//		List<Product> productsList = productController.getAllProducts();
+//		model.addAttribute("products", productsList);
+//		return "admin_product";
+//	}
 	
 	@GetMapping("/add_product")
 	public String addProduct(Model model) {
@@ -47,7 +74,7 @@ public class WebController {
 	@PostMapping("/save_product")
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		productController.addProduct(product);
-		return "redirect:/";
+		return "redirect:/product";
 	}
 	
 	@GetMapping("/contact")
--- E-Healthcare/src/main/resources/static/js/script.js
+++ E-Healthcare/src/main/resources/static/js/script.js
@@ -1,8 +1,34 @@
+/* Search Script */
 function takeInput() {
 	var search = document.getElementById("searchBar");
 	console.log(search.value);
 }
 
+/* Number of Entries Script */
 function showEntries() {
-	console.log("change occurred");
+	var numChosen = document.getElementById("numEntries").value;
+	
+	let data = {numChosen: numChosen};
+
+	fetch("/productChange", {
+	  method: "POST",
+	  headers: {'Content-Type': 'application/json'}, 
+	  body: JSON.stringify(data)
+	}).then(res => {
+	  console.log("Request complete! response:", res);
+	});
+	
+	/*
+	Look at th:fragment 
+	https://www.baeldung.com/spring-thymeleaf-fragments
+	https://frontbackend.com/thymeleaf/how-to-work-with-fragments-in-thymeleaf
+	*/
+	
+	/*setTimeout(() => {
+		location.reload();
+	}, 500)
+
+	console.log(numChosen2);
+	document.querySelector('#numEntries option[value="' + numChosen2 +'"]').selected = true;*/
+
 }
