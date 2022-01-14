ec1ff0b0b1c42118e216fc0427c171dde86bdaa7
Landon Wyatt <landon@test.com>
2022-01-14 10:47:14 -0500

    products page connection to DB

--- E-Healthcare/src/main/java/com/mphasis/EHealthcareApplication.java
+++ E-Healthcare/src/main/java/com/mphasis/EHealthcareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EHealthcareApplication {
+public class EHealthcareApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EHealthcareApplication.class, args);
 	}
-
+	
 }
--- E-Healthcare/src/main/java/com/mphasis/controller/WebController.java
+++ E-Healthcare/src/main/java/com/mphasis/controller/WebController.java
@@ -1,10 +1,21 @@
 package com.mphasis.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mphasis.model.Product;
 
 @Controller
 public class WebController {
+	
+	@Autowired
+	private ProductController productController;
 	
 	@GetMapping("/")
 	public String getHome() {
@@ -17,9 +28,26 @@ public class WebController {
 	}
 	
 	@GetMapping("/product")
-	public String getProduct() {
-		// user_product or admin_product
-		return "user_product";
+	public String getProduct(Model model) {
+		// user_product or admin_product needs to be determined and decide which to return
+//		int numEntries = 5;
+		System.out.println("change occurred");
+		List<Product> productsList = productController.getAllProducts();
+		model.addAttribute("products", productsList);
+		return "admin_product";
+	}
+	
+	@GetMapping("/add_product")
+	public String addProduct(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		return "add_product";
+	}
+	
+	@PostMapping("/save_product")
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		productController.addProduct(product);
+		return "redirect:/";
 	}
 	
 	@GetMapping("/contact")
